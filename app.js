dbc670f1c94d64e14f7fb56b7128226819068af2
mofengfs <mofengfs@126.com>
2019-02-03 16:11:15 +0800

    结合 art-template 模板实现用户管理 显示 增加 编辑 删除

--- app.js
+++ app.js
@@ -20,33 +20,126 @@ var router = new Router();
 
 /**配置中间件  -- 第三方中间件 */
 
-render(app, {
+render(app, { /**配置 art-template 模板引擎 */
     root: path.join(__dirname, 'views'), //视图位置
     extname: '.html', //后缀名
     debug: process.env.NODE_ENV !== 'production'  //是否开启调试模式
 })
+
+app.use(bodyParser()) /**配置 POST 提交中间件 */
 
 
 /**配置路由 */
 
 router.get('/', async (ctx) => {
 
-    console.time('start1')
     var result = await DB.find('user', {})
-    console.timeEnd('start1')
 
-    ctx.body = result
+    await ctx.render('index', {
 
+        userList: {
+
+            result
+
+        }
+
+    })
 
 })
 
-router.get('/news', async (ctx) => {
 
-    console.time('start2')
-    var result = await DB.find('user', {})
-    console.timeEnd('start2')
+router.get('/add', async (ctx) => {
 
-    ctx.body = result
+    await ctx.render('add')
+
+})
+
+router.post('/doAdd', async (ctx) => {
+
+    let data = await DB.insert('user', ctx.request.body)
+
+    try {
+
+        if (data.result.ok) {
+
+            ctx.redirect('/')
+
+        }
+
+    } catch (err) {
+
+        console.log(err)
+
+    }
+
+})
+
+router.get('/edit', async (ctx) => {
+
+    //通过get传过来的id来获取用户信息
+
+    let id = ctx.query.id
+
+    let data = await DB.find('user', { "_id": DB.getObjectID(id) })
+
+    await ctx.render('edit', {
+
+        userList: data[0]
+
+    })
+
+})
+
+router.post('/doEdit', async (ctx) => {
+
+    console.log(ctx.request.body)
+
+    let id = ctx.request.body.id
+    let username = ctx.request.body.username
+    let age = ctx.request.body.age
+    let sex = ctx.request.body.sex
+
+    let data = await DB.update('user', { "_id": DB.getObjectID(id) }, {
+
+        username, age, sex
+
+    })
+
+    try {
+
+        if (data.result.ok) {
+
+            ctx.redirect('/')
+
+        }
+
+    } catch (err) {
+
+        console.log(err)
+
+    }
+
+})
+
+router.get('/delete', async (ctx) => {
+
+    let id = ctx.query.id
+
+    let data = await DB.remove('user', { "_id": DB.getObjectID(id) })
+
+    try {
+
+        if (data.result.ok) {
+
+            ctx.redirect('/')
+
+        }
+
+    } catch (err) {
+
+        console.log(err)
+
+    }
 
 })
 
--- module/db.js
+++ module/db.js
@@ -2,13 +2,15 @@
 
 const MongoClient = require('mongodb').MongoClient;
 
+const ObjectID = require('mongodb').ObjectID;
+
 const Config = require('./config.js');
 
 class Db {
 
     static getInstance() { // 1.单例  解决多次实例化, 实例不共享的问题
 
-        if(!Db.instance) {
+        if (!Db.instance) {
 
             Db.instance = new Db()
 
@@ -60,7 +62,7 @@ class Db {
 
     }
 
-    find(collectionName, json) {
+    find(collectionName, json) { /**查找数据 */
 
         return new Promise((resolve, reject) => {
 
@@ -88,11 +90,92 @@ class Db {
 
     }
 
-    update() {
+    insert(collectionName, json) { /**增加数据 */
+
+        return new Promise((resolve, reject) => {
+
+            this.connect().then((db) => {
+
+                db.collection(collectionName).insertOne(json, (err, result) => {
+
+                    if (err) {
+
+                        reject(err)
+
+                    } else {
+
+                        resolve(result)
+
+                    }
+
+                })
+
+            })
+
+        })
 
     }
 
-    insert() {
+    update(collectionName, json1, json2) { /**更改数据 */
+
+        return new Promise((resolve, reject) => {
+
+            this.connect().then((db) => {
+
+                db.collection(collectionName).updateOne(json1, {
+
+                    $set: json2
+
+                }, (err, result) => {
+
+                    if (err) {
+
+                        reject(err)
+
+                    } else {
+
+                        resolve(result)
+
+                    }
+
+                })
+
+            })
+
+        })
+
+    }
+
+
+    remove(collectionName, json) { /**删除数据 */
+
+        return new Promise((resolve, reject) => {
+
+            this.connect().then((db) => {
+
+                db.collection(collectionName).removeOne(json, (err, result) => {
+
+                    if (err) {
+
+                        reject(err)
+
+                    } else {
+
+                        resolve(result)
+
+                    }
+
+                })
+
+            })
+
+        })
+
+    }
+
+    getObjectID(id) { /**mongodb里面查询 _id 把字符串转换成对象 */
+
+        return new ObjectID(id) 
 
     }
 
